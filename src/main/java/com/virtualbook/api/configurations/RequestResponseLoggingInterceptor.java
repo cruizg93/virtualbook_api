1625a135bc05d0758038ae07690c711878062b84
Cristian Ruiz Gonzalez <crisruiz93@gmail.com>
2018-08-24 16:48:38 -0500

    Layers, exceptions and loggs

---
+++ src/main/java/com/virtualbook/api/configurations/RequestResponseLoggingInterceptor.java
@@ -0,0 +1,52 @@
+package com.virtualbook.api.configurations;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.util.StreamUtils;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class RequestResponseLoggingInterceptor implements ClientHttpRequestInterceptor{
+	
+	@Override
+	public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+
+		logRequest(request,body);
+		ClientHttpResponse response = execution.execute(request, body);
+		logResponse(response);
+		return response;
+	}
+
+	
+	private void logRequest(HttpRequest request, byte[] body) throws IOException{
+		if(log.isDebugEnabled()) {
+			StringBuilder sb = new StringBuilder();
+			sb.append("=======================REQUEST BEGIN================================");
+			sb.append("\n URI           :").append(request.getURI());
+			sb.append("\n Method        :").append(request.getMethod());
+			sb.append("\n Headers       :").append(request.getHeaders());
+			sb.append("\n Request Body  :").append(new String(body, "UTF-8"));
+			sb.append("=======================REQUEST END==================================");
+			log.debug(sb.toString());
+		}
+	}
+	
+	private void logResponse(ClientHttpResponse response) throws IOException{
+		if(log.isDebugEnabled()) {
+			StringBuilder sb = new StringBuilder();
+			sb.append("=======================RESPONSE BEGIN================================");
+			sb.append("\n Status Code   :").append(response.getStatusCode());
+			sb.append("\n Status text   :").append(response.getStatusText());
+			sb.append("\n Headers       :").append(response.getHeaders());
+			sb.append("\n Response Body :").append(StreamUtils.copyToString(response.getBody(), Charset.defaultCharset()));
+			sb.append("=======================RESPONSE END==================================");
+			log.debug(sb.toString());
+		}
+	}
+}
---
+++ src/main/java/com/virtualbook/api/configurations/RestClientConfig.java
@@ -0,0 +1,29 @@
+package com.virtualbook.api.configurations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestClientConfig {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+		
+		List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
+		
+		if(CollectionUtils.isEmpty(interceptors)) {
+			interceptors = new ArrayList<>();
+		}
+		interceptors.add(new RequestResponseLoggingInterceptor());
+		restTemplate.setInterceptors(interceptors);
+		return restTemplate;
+	}
+}
