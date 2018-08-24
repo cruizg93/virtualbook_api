5d400cef5ef26af50b87fca1467dba947e242fbf
Cristian Ruiz Gonzalez <crisruiz93@gmail.com>
2018-08-24 16:28:58 -0500

    Initial Commit with DB connection

---
+++ src/main/java/com/virtualbook/api/VirtualbookApiApplication.java
@@ -0,0 +1,12 @@
+package com.virtualbook.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualbookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualbookApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/virtualbook/api/VirtualbookApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.virtualbook.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VirtualbookApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
